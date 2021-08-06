fd563aae4bfb7148b4b02f0d0d70ad80cdb62fe6
ErlingFX <ErlingFX@gmail.com>
2021-08-06 17:22:13 +0400

    added mock DB, + refactor

---
+++ MVPTest/DB/DataBase.swift
@@ -0,0 +1,20 @@
+//
+//  DataBase.swift
+//  MVPTest
+//
+//  Created by Alexander Nazarov on 06.08.2021.
+//
+
+import Foundation
+
+class DataBase {
+    static var shared = DataBase()
+    private var arrayOfEvents: [Task] = []
+    
+    func getArrayOfEvent() -> [Task] {
+        return arrayOfEvents
+    }
+    func addArrayOfEvent(addTask: Task) {
+        arrayOfEvents.append(addTask)
+    }
+}
