b40713066e999bf05c0f5f0a2c4cf533eac1ddfd
ErlingFX <ErlingFX@gmail.com>
2021-08-06 14:55:53 +0400

    added push to screen EventVC

---
+++ MVPTest/Extension/Date.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  MVPTest
+//
+//  Created by Alexander Nazarov on 06.08.2021.
+//
+
+import Foundation
+
+extension DateFormatter {
+    
+    static let created: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+        return formatter
+    }()
+}
