102fe190b397fa385ef8a512e8673e84334a67db
ErlingFX <ErlingFX@gmail.com>
2021-08-10 15:36:39 +0400

    create datebase SQLite

---
+++ MVPTest/SQLiteCommands.swift
@@ -0,0 +1,8 @@
+//
+//  SQLiteCommands.swift
+//  MVPTest
+//
+//  Created by Alexander Nazarov on 10.08.2021.
+//
+
+import Foundation
---
+++ MVPTest/SQLiteDatabase.swift
@@ -0,0 +1,8 @@
+//
+//  SQLiteDatabase.swift
+//  MVPTest
+//
+//  Created by Alexander Nazarov on 09.08.2021.
+//
+
+import Foundation
