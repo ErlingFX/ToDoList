aa90905164286f6059965038ebcbb368ce2a8a07
ErlingFX <ErlingFX@gmail.com>
2021-08-12 16:39:24 +0400

    add deleting table row in database + all refactor

--- MVPTest/Extension/Date.swift
+++ MVPTest/Extension/Date.swift
@@ -1,18 +1,8 @@
 //
-//  File.swift
+//  Date.swift
 //  MVPTest
 //
-//  Created by Alexander Nazarov on 06.08.2021.
+//  Created by Alexander Nazarov on 12.08.2021.
 //
 
 import Foundation
-
-extension DateFormatter {
-    
-    static let created: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        formatter.dateStyle = .medium
-        return formatter
-    }()
-}
