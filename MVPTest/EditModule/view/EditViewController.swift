591e692b914d7d45226f755e443120a659bfdbcc
Erling <Erlingfx@gmail.com>
2021-08-26 17:56:07 +0400

    refactor editVC

--- MVPTest/EditModule/view/EditViewController.swift
+++ MVPTest/EditModule/view/EditViewController.swift
@@ -8,50 +8,52 @@
 import UIKit
 
 class EditViewController: UIViewController, UITextFieldDelegate{
-    
     var presenter: EditViewPresenterProtocol!
     
-    
+    //MARK: - IBOutlet
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var editTextFiled: UITextField!
-    //---
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var editImage: UIImageView!
     @IBOutlet weak var editNameEventLabel: UILabel!
     @IBOutlet weak var editButtonLabel: UIButton!
     @IBOutlet weak var addImageLabel: UIButton!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.transferDataTest()
-        
     }
-    
     override func viewWillAppear(_ animated: Bool) {
         editTextFiled.center.x += view.bounds.width
         saveButton.center.x += view.bounds.width
         configureAnimate()
     }
     
-    
     //MARK: - Кнопка вызова и изменения текста в таске
     @IBAction func editButtonAction(_ sender: UIButton) {
         moveTo()
     }
-    //MARK: - Кнопка сохранения для текстфилда
     
+    //MARK: - Кнопка сохранения для текстфилда
     @IBAction func editTFsaveButt(_ sender: UIButton) {
-        if editTextFiled
-        guard let text = self.editTextFiled.text else { return }
-        self.editNameEventLabel.text = text
-        self.presenter.task.nameEvent = text
-        self.presenter.updateRowFromSQLiteDatabase(task: self.presenter.task)
-        moveToBack()
-        editTextFiled.resignFirstResponder()
-        editTextFiled.text = ""
+        // если текст есть то
+        if editTextFiled.hasText {
+            guard let text = self.editTextFiled.text else { return }
+            self.editNameEventLabel.text = text
+            self.presenter.task.nameEvent = text
+            self.presenter.updateRowFromSQLiteDatabase(task: self.presenter.task)
+            moveToBack()
+            editTextFiled.resignFirstResponder()
+            editTextFiled.text = ""
+            //если нету
+        } else {
+            notextTF()
+        }
     }
 }
 
+//MARK: - datePickerVC
 extension EditViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
     @IBAction func addImageAction(_ sender: Any) {
@@ -76,6 +78,21 @@ extension EditViewController: UIImagePickerControllerDelegate & UINavigationCont
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: - no text in TF animate
+    func notextTF() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.07
+        animation.repeatCount = 4
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: editTextFiled.center.x - 10, y: editTextFiled.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: editTextFiled.center.x + 10, y: editTextFiled.center.y))
+        editTextFiled.layer.add(animation, forKey: "position")
+        editTextFiled.placeholder = "not input text"
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+            self.editTextFiled.placeholder = "enter your task"
+        }
     }
     
     //MARK: - animate
