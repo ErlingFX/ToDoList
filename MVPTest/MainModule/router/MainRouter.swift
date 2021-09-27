317259746b8119b17551f5953907bd6746c08b0d
Erling <Erlingfx@gmail.com>
2021-09-27 14:17:22 +0400

    Initial refactor branch

--- MVPTest/MainModule/router/MainRouter.swift
+++ MVPTest/MainModule/router/MainRouter.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 
+//MainRouterProtocol
 protocol MainRouterMain {
     var viewController: UINavigationController { get set }
     var assamblyBuilder: AssamblyBuilderProtocol { get set }
